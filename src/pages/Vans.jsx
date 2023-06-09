e4cc5d6c6eb28226da4099f7184b13355b868a75
Carlos Cespedes <cespedes88@gmail.com>
2023-06-08 22:29:31 -0700

    change data source and add vans page

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,13 +1,32 @@
 import { useState, useEffect } from "react";
+import vanData from "/vanData.js";
 
 export default function Vans() {
   const [vans, setVans] = useState([]);
-  //const vans = [];
+
   useEffect(() => {
-    fetch("api/vans")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+    setVans(vanData.data.vans);
+    // fetch("/api/vans")
+    //   .then((res) => res.json())
+    //   .then((data) => console.log(data));
   }, []);
-  //console.log(vans);
-  return <h1>Vans page goes here 🚐</h1>;
+
+  const vanElements = vans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} />
+      <div className="van-info-text">
+        <h3>{van.name}</h3>
+        <p>${van.price}/day</p>
+      </div>
+      <div className={`van-type ${van.type} selected`}>{van.type}</div>
+    </div>
+  ));
+
+  console.log(vans);
+  return (
+    <div className="main-container">
+      <h1>Explore our van options</h1>
+      <div className="van-container">{vanElements}</div>
+    </div>
+  );
 }
